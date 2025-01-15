c7d181fb49e08d692913400c94d90b99a100a5a1
Roderick Esquivle <roderickenrique42@gmail.com>
2025-01-15 09:28:32 -0500

    new font style added

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './pages/Login.jsx'
-import login from './pages/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,38 +6,38 @@ module.exports = {
 		borderRadius: {
 		  lg: 'var(--radius)',
 		  md: 'calc(var(--radius) - 2px)',
-		  sm: 'calc(var(--radius) - 4px)'
+		  sm: 'calc(var(--radius) - 4px)',
 		},
 		colors: {
 		  background: 'hsl(var(--background))',
 		  foreground: 'hsl(var(--foreground))',
 		  card: {
 			DEFAULT: 'hsl(var(--card))',
-			foreground: 'hsl(var(--card-foreground))'
+			foreground: 'hsl(var(--card-foreground))',
 		  },
 		  popover: {
 			DEFAULT: 'hsl(var(--popover))',
-			foreground: 'hsl(var(--popover-foreground))'
+			foreground: 'hsl(var(--popover-foreground))',
 		  },
 		  primary: {
 			DEFAULT: 'hsl(var(--primary))',
-			foreground: 'hsl(var(--primary-foreground))'
+			foreground: 'hsl(var(--primary-foreground))',
 		  },
 		  secondary: {
 			DEFAULT: 'hsl(var(--secondary))',
-			foreground: 'hsl(var(--secondary-foreground))'
+			foreground: 'hsl(var(--secondary-foreground))',
 		  },
 		  muted: {
 			DEFAULT: 'hsl(var(--muted))',
-			foreground: 'hsl(var(--muted-foreground))'
+			foreground: 'hsl(var(--muted-foreground))',
 		  },
 		  accent: {
 			DEFAULT: 'hsl(var(--accent))',
-			foreground: 'hsl(var(--accent-foreground))'
+			foreground: 'hsl(var(--accent-foreground))',
 		  },
 		  destructive: {
 			DEFAULT: 'hsl(var(--destructive))',
-			foreground: 'hsl(var(--destructive-foreground))'
+			foreground: 'hsl(var(--destructive-foreground))',
 		  },
 		  border: 'hsl(var(--border))',
 		  input: 'hsl(var(--input))',
@@ -47,22 +47,25 @@ module.exports = {
 			'2': 'hsl(var(--chart-2))',
 			'3': 'hsl(var(--chart-3))',
 			'4': 'hsl(var(--chart-4))',
-			'5': 'hsl(var(--chart-5))'
-  			},
-  			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: '#fff',
-  				primary: '#343a40',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
+			'5': 'hsl(var(--chart-5))',
 		  },
-		  darkGrey: '#363636' 
-		}
-	  }
+		  sidebar: {
+			DEFAULT: 'hsl(var(--sidebar-background))',
+			foreground: '#fff',
+			primary: '#343a40',
+			'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+			accent: 'hsl(var(--sidebar-accent))',
+			'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+			border: 'hsl(var(--sidebar-border))',
+			ring: 'hsl(var(--sidebar-ring))',
+		  },
+		  darkGrey: '#363636',
+		},
+		fontFamily: {
+		  sans: ['Urbanist', 'sans-serif'], // Fuente Urbanist agregada aquí
+		},
+	  },
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [require('tailwindcss-animate')],
   };
   
